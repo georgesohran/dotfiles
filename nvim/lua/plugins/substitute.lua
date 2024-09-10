f2d09fa6285eef62588eb3117db82022f93a471f
georgesohran <soxrgeorge@gmail.com>
2024-09-10 15:04:38 +0500

    trying to fix substitude

--- nvim/lua/plugins/substitute.lua
+++ nvim/lua/plugins/substitute.lua
@@ -2,7 +2,7 @@ return {
   "gbprod/substitute.nvim",
   config = function()
     local substitute = require('substitute')
-    vim.keymap.set('n', 's', substitute.operator, {})
-    vim.keymap.set('v', 's', substitute.visual, {})
+    vim.keymap.set('n', 's', substitute.operator, { noremap = true })
+    vim.keymap.set('v', 's', substitute.visual, {noremap = true })
   end
 }
