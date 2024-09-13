6b64d212e7d529ba88f1e529460a37ea766be937
georgesohran <soxrgeorge@gmail.com>
2024-09-13 21:28:42 +0500

    starting work on my custom stuff

---
+++ nvim/lua/utils.lua
@@ -0,0 +1,17 @@
+return {
+  get_floating_window = function (width, height)
+    
+    local buf = vim.api.nvim_create_buf(true, true)
+    local ui = vim.api.nvim_list_uis()[0]
+    local window = vim.api.nvim_open_win(buf, 1, {
+      relative = 'editor',
+      width = width,
+      height = height,
+      col = (ui.width/2) - (width/2),
+      row = (ui.height/2) - (height/2),
+      anchor = 'NW',
+      style = 'minimal',
+      border = 'rounded'
+    })
+  end
+}
