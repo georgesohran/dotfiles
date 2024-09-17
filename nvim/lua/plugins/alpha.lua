a5b188e353c7ddd812171e727d6fb6a3e5f2d4a0
georgesohran <soxrgeorge@gmail.com>
2024-09-17 09:21:47 +0500

    editing alpha.lua

--- nvim/lua/plugins/alpha.lua
+++ nvim/lua/plugins/alpha.lua
@@ -6,16 +6,15 @@ return {
     local dashboard = require('alpha.themes.dashboard')
 
     dashboard.section.header.val = {
-      '      ::::    :::  ::::::::::  ::::::::     :::     :::  :::::::::::    :::   :::', 
-      '     :+:+:   :+:  :+:        :+:    :+:    :+:     :+:      :+:       :+:+: :+:+:', 
-      '    :+:+:+  +:+  +:+        +:+    +:+    +:+     +:+      +:+      +:+ +:+:+ +:+', 
-      '   +#+ +:+ +#+  +#++:++#   +#+    +:+    +#+     +:+      +#+      +#+  +:+  +#+ ', 
-      '  +#+  +#+#+#  +#+        +#+    +#+     +#+   +#+       +#+      +#+       +#+  ', 
-      ' #+#   #+#+#  #+#        #+#    #+#      #+#+#+#        #+#      #+#       #+#   ', 
-      '###    ####  ##########  ########         ###      ###########  ###       ###    ', 
+      '      ::::    :::  ::::::::::  ::::::::     :::     :::  :::::::::::    :::   :::',
+      '     :+:+:   :+:  :+:        :+:    :+:    :+:     :+:      :+:       :+:+: :+:+:',
+      '    :+:+:+  +:+  +:+        +:+    +:+    +:+     +:+      +:+      +:+ +:+:+ +:+',
+      '   +#+ +:+ +#+  +#++:++#   +#+    +:+    +#+     +:+      +#+      +#+  +:+  +#+ ',
+      '  +#+  +#+#+#  +#+        +#+    +#+     +#+   +#+       +#+      +#+       +#+  ',
+      ' #+#   #+#+#  #+#        #+#    #+#      #+#+#+#        #+#      #+#       #+#   ',
+      '###    ####  ##########  ########         ###      ###########  ###       ###    ',
     }
     dashboard.section.buttons.val = {
-      dashboard.button("e", "  > New File", "<cmd>ene<CR>"),
       dashboard.button("SPC ee", "  > Toggle file explorer", "<cmd>NvimTreeToggle<CR>"),
       dashboard.button("SPC ff", "󰱼  > Find File", "<cmd>Telescope find_files<CR>"),
       dashboard.button("SPC fs", "  > Find Word", "<cmd>Telescope live_grep<CR>"),
@@ -23,8 +22,6 @@ return {
     }
 
     alpha.setup(dashboard.opts)
-
-    vim.cmd([[autocmd FileType alpha setlocal nofoldenble]])
   end,
 }
 
