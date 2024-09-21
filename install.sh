93d179a6e7bcf09ff1324ed0379b810c1a7e2e76
georgesohran <soxrgeorge@gmail.com>
2024-09-21 18:16:25 +0500

    adding to install.sh the install of neovim

--- install.sh
+++ install.sh
@@ -3,3 +3,7 @@
 ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/.tmux ~/.tmux
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
+
+curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
+chmod u+x nvim.appimage
+mv nvim.appimage /bin/nvim
