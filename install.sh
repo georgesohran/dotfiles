8bcbf5728f06a90335283452bf2c8a047108aaf1
georgesohran <soxrgeorge@gmail.com>
2024-09-21 20:42:33 +0500

    addingtmux installation in install.sh

--- install.sh
+++ install.sh
@@ -4,6 +4,15 @@ ln -s ~/.dotfiles/nvim ~/.config/nvim
 ln -s ~/.dotfiles/.tmux ~/.tmux
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 
+# installing neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
 chmod u+x nvim.appimage
 mv nvim.appimage /bin/nvim
+
+# installing tmux
+git clone https://github.com/tmux/tmux.git
+cd tmux
+sh autogen.sh
+./configure
+make && sudo make install
+
