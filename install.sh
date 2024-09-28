b2fc7d33d48fa1a2e7e36e16b6b0f56695a51861
georgesohran <soxrgeorge@gmail.com>
2024-09-28 15:39:48 +0500

    updating install script (what's the point... it is not gona work properly anyway)

--- install.sh
+++ install.sh
@@ -1,18 +1,19 @@
 #/bin/bash
 
-ln -s ~/.dotfiles/nvim ~/.config/nvim
-ln -s ~/.dotfiles/.tmux ~/.tmux
+ln -s ~/.dotfiles/.config/nvim ~/.config/nvim
+ln -s ~/.dotfiles/.config/alacritty ~/.config/alacritty
+ln -s ~/.dotfiles/.tmux.conf ~/.tmux.conf
 ln -s ~/.dotfiles/.bashrc ~/.bashrc
 
 # installing neovim
 curl -LO https://github.com/neovim/neovim/releases/latest/download/nvim.appimage
-chmod u+x nvim.appimage
-mv nvim.appimage /bin/nvim
+sudo chmod u+x nvim.appimage
+sudo mv nvim.appimage /bin/nvim
 
 # installing tmux
-git clone https://github.com/tmux/tmux.git
-cd tmux
-sh autogen.sh
-./configure
-make && sudo make install
+# git clone https://github.com/tmux/tmux.git
+# cd tmux
+# sh autogen.sh
+# ./configure
+# make && sudo make install
 
