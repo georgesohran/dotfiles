36df77930f5b05d4806b4374c34d58ee115fa69e
georgesohran <soxrgeorge@gmail.com>
2024-09-28 20:07:06 +0500

    changing file structure

--- README.md
+++
@@ -1,6 +0,0 @@
-# DOTFILES
-
-#### Instalation guide
-
-first of all install tmux and nvim, this step is on your own, because there is a huge variety of distros out there
-
--- nvim/lua/plugins/substitute.lua
+++
@@ -1,8 +0,0 @@
-return {
-  "gbprod/substitute.nvim",
-  config = function()
-    local substitute = require('substitute')
-    vim.keymap.set('n', 's', substitute.operator, { noremap = true })
-    vim.keymap.set('v', 's', substitute.visual, {noremap = true })
-  end
-}
