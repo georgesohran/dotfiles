13f39264d010b554af253c4b6730dc1e3f6ceedf
georgesohran <soxrgeorge@gmail.com>
2024-10-11 21:49:44 +0500

    adding highlighting for yanked text

--- .config/nvim/lua/options.lua
+++ .config/nvim/lua/options.lua
@@ -35,4 +35,12 @@ vim.diagnostic.config({
   virtual_text = false
 })
 
+vim.api.nvim_create_autocmd('TextYankPost', {
+  group = vim.api.nvim_create_augroup('highlight_yank', {}),
+  pattern = '*',
+  callback = function()
+    vim.highlight.on_yank { higroup = 'IncSearch', timeout = 200 }
+  end,
+})
+
 
