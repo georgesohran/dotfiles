b5559643bea459e921732666ba2ac336177ce0e4
georgesohran <soxrgeorge@gmail.com>
2024-10-26 20:31:08 +0500

    configuring lsp... again

--- .config/nvim/lua/plugins/lsp-stuff.lua
+++ .config/nvim/lua/plugins/lsp-stuff.lua
@@ -15,8 +15,9 @@ return {
           'tailwindcss',
           'pyright',
           'lua_ls',
-          'rust_analyzer',
-          -- 'emmet_ls'
+          -- 'rust_analyzer',
+          'cssls',
+          'gopls'
         }
       })
     end
@@ -24,12 +25,12 @@ return {
   {
     'neovim/nvim-lspconfig',
     config = function()
-      vim.lsp.set_log_level("debug")
+      -- vim.lsp.set_log_level("debug")
 
       local capabilities = vim.lsp.protocol.make_client_capabilities()
       capabilities.textDocument.completion.completionItem.snippetSupport = true
 
-      local configs = require('lspconfig/configs')
+      -- local configs = require('lspconfig/configs')
 
       local lsp_conf = require('lspconfig')
       lsp_conf.ts_ls.setup({
@@ -37,25 +38,17 @@ return {
         filetypes = {
           'javascriptreact',
           'javascript',
-          'javascript.jsx'
+          'javascript.jsx',
+          'css'
         }
       })
       lsp_conf.html.setup({capabilities=capabilities})
       lsp_conf.tailwindcss.setup({capabilities = capabilities})
       lsp_conf.pyright.setup({capabilities = capabilities})
       lsp_conf.lua_ls.setup({capabilities = capabilities})
-      lsp_conf.rust_analyzer.setup({capabilities = capabilities})
-      -- lsp_conf.emmet_ls.setup({
-      --   capabilities = capabilities,
-      --   filetypes = { "css", "html", "javascript", "javascriptreact", "less", "sass", "scss", "svelte", "typescriptreact", "vue" },
-      --   init_options = {
-      --     html = {
-      --       options = {
-      --         ["bem.enabled"] = true,
-      --       },
-      --     },
-      --   }}
-      -- )
+      -- lsp_conf.rust_analyzer.setup({capabilities = capabilities})
+      lsp_conf.cssls.setup({capabilities = capabilities})
+      lsp_conf.gopls.setup({capabilities = capabilities})
 
       vim.keymap.set('n', 'K', vim.lsp.buf.hover, {})
       vim.keymap.set('n', 'gd', vim.lsp.buf.definition, {})
